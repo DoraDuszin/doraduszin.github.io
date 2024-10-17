d492ceda926906b33682fb19fba010f0f2d55604
Dora Duszin <dora.duszin@gmail.com>
2024-10-17 23:09:04 +0200

    Video header, home component.

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ComingSoonComponent} from "./features/coming-soon/coming-soon/coming-soon.component";
+import {HomeComponent} from "./features/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ComingSoonComponent],
+    imports: [RouterOutlet, ComingSoonComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

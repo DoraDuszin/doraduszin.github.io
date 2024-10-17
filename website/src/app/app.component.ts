91935bcbd6437887cc985de189c77bf57b5ceab9
Dora Duszin <dora.duszin@gmail.com>
2024-10-17 20:47:57 +0200

    Coming soon.

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ComingSoonComponent} from "./features/coming-soon/coming-soon/coming-soon.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ComingSoonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

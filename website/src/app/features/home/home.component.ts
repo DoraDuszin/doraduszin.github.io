9f03cbfd27c43060fb1ca324d496ba275377f53d
Dora Duszin <dora.duszin@gmail.com>
2024-10-23 23:37:43 +0200

    Navbar and header.

--- website/src/app/features/home/home.component.ts
+++ website/src/app/features/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {NavbarComponent} from "../../layout/navbar/navbar.component";
+import {HeaderComponent} from "../../layout/header/header.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent,
+    HeaderComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

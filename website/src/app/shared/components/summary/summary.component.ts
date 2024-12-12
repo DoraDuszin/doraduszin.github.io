02970e9cf5686a32eed41e11c15647310bd6ae3e
Dora Duszin <dora.duszin@gmail.com>
2024-12-12 23:25:39 +0100

    Test.

--- website/src/app/shared/components/summary/summary.component.ts
+++ website/src/app/shared/components/summary/summary.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-summary',
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.css'
 })
-export class SummaryComponent {
+export class SummaryComponent implements OnInit {
+
+  activeServiceIndex = 0; // Track which service is active
+  services: HTMLElement[] = []; // To hold the service elements
+
+  ngOnInit() {
+    this.initializeCarousel();
+  }
+
+  initializeCarousel() {
+    this.services = Array.from(document.querySelectorAll('.service'));
+    this.showService(this.activeServiceIndex);
+
+    // Set up the interval for switching services
+    setInterval(() => {
+      this.activeServiceIndex = (this.activeServiceIndex + 1) % this.services.length;
+      this.showService(this.activeServiceIndex);
+    }, 2000); // Change service every 2 seconds
+  }
+
+  showService(index: number) {
+    this.services.forEach((service, i) => {
+      service.classList.toggle('active', i === index); // Add 'active' class to the visible service
+    });
+  }
 
 }
